38fb931985eb98212ae41c1a3cdeb03fc7e02598
GCout <Gccoutret@gmail.com>
2019-09-19 15:09:07 -0500

    added function to search giphy for searched string automatically

--- assets/giphy.js
+++ assets/giphy.js
@@ -3,32 +3,37 @@ $(document).ready(function(){
   var searches= ['hotdog','pizza','bagel', 'wink'];
 
   $(document).on('click', '.search-btn', function(){
-     var apiKey = "api_key=pzS1gd5OekBfGA7piNtcDxbVkSiac1aL";
-  var url = "https://api.giphy.com/v1/gifs/search?";
   
     var search= $(this).text()
-    url= url + "limit=10"+ "&"+ "q=" +search + "&"+ apiKey;
-    $.ajax({
-      method: "GET",
-      url: url,
+    callApi(search)
+  })
+  function callApi(populate){
+     var apiKey = "api_key=pzS1gd5OekBfGA7piNtcDxbVkSiac1aL";
+  var url = "https://api.giphy.com/v1/gifs/search?";
+  var q=populate
+  url= url + "limit=10"+ "&"+ "q=" +populate + "&"+ apiKey;
+  $.ajax({
+    method: "GET",
+    url: url,
 
-    }).then(function(response){
-      $(".gif-div").empty();
-      console.log(response)
-      response.data.forEach(function(gif){
-        var gifAppend = $("<div>")
-        var rating = $("<p>")
-        var gifImg= $("<img>")
+  }).then(function(response){
+    $(".gif-div").empty();
+    console.log(response)
+    response.data.forEach(function(gif){
+      var gifAppend = $("<div>")
+      var rating = $("<p>")
+      var gifImg= $("<img>")
 
-        rating.text(gif.rating)
-        gifImg.attr("src", gif.images.fixed_height.url);
-        gifAppend.append(rating).append(gifImg)
-        $(".gif-div").append(gifAppend)
-      
-      })
+      rating.text(gif.rating)
+      gifImg.attr("src", gif.images.fixed_height.url);
+      gifAppend.append(rating).append(gifImg)
+      $(".gif-div").append(gifAppend)
     
-  })
-  })
+    })
+  
+})
+
+  }
   function makeButtons() {
     $(".button-container").empty();
 
@@ -42,13 +47,13 @@ $(document).ready(function(){
   };
 
   $("#add-search").on("click", function(button) {
-    console.log("im in the function")
     event.preventDefault();
     var gifSearch = $("#search-input").val().trim();
+    callApi(gifSearch);
      if(!(searches.includes(gifSearch))){
-    searches.push(gifSearch);
+    searches.push(gifSearch)
     makeButtons();
-    console.log(gifSearch)}
+    }
   });
 
   makeButtons()
